9f797941a09f7c94ece1ec23d16ba48c3af9bb1f
Reese <reese.westerhoff@gmail.com>
2025-10-23 12:48:00 +0100

    avoid useless renaming, create index file in controllers directory

--- back-end/services/bp/src/routes/bloodPressureRoutes.ts
+++ back-end/services/bp/src/routes/bloodPressureRoutes.ts
@@ -1,22 +1,18 @@
 // Description: API routes for blood pressure readings.
 
-import expressRoute, { Request, Response, NextFunction } from "express";
+import express from "express";
 import {
-  addReading as addReadingController,
-  getReadings as getReadingsController,
-  getAverageBloodPressure as getAverageBloodPressureController,
-} from "../controllers/bloodPressureController";
-import { ensureAuth as ensureAuthMiddleware } from "../middleware";
+  addReading,
+  getReadings,
+  getAverageBloodPressure,
+} from "../controllers";
+import { ensureAuth } from "../middleware";
 
-const bloodPressureRoutes = expressRoute.Router();
+const bloodPressureRoutes = express.Router();
 
 // All routes in this file are protected and require authentication
-bloodPressureRoutes.post("/", ensureAuthMiddleware, addReadingController);
-bloodPressureRoutes.get("/", ensureAuthMiddleware, getReadingsController);
-bloodPressureRoutes.get(
-  "/average",
-  ensureAuthMiddleware,
-  getAverageBloodPressureController
-);
+bloodPressureRoutes.post("/", ensureAuth, addReading);
+bloodPressureRoutes.get("/", ensureAuth, getReadings);
+bloodPressureRoutes.get("/average", ensureAuth, getAverageBloodPressure);
 
 export { bloodPressureRoutes };
