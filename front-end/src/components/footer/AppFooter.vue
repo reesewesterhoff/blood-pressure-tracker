b7c60cbf3867ed483f2120b227503b15286da049
Reese <reese.westerhoff@gmail.com>
2025-11-05 15:25:23 +0100

    create footer

---
+++ front-end/src/components/footer/AppFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer
+    class="w-full bg-neutral-200 dark:bg-neutral-950 border-t border-neutral-300 dark:border-neutral-800 mt-auto px-4 sm:px-6 lg:px-8 py-2 flex justify-center items-center"
+  >
+    <p class="text-sm text-neutral-600 dark:text-neutral-400">
+      © 2024 BP Tracker. All rights reserved.
+    </p>
+  </footer>
+</template>
