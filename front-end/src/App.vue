132726e8c2297856dcd2629737931fcabc89d9ce
Reese <reese.westerhoff@gmail.com>
2025-11-05 15:26:34 +0100

    implement theme toggle, navbar, header, and footer into app

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import AppHeader from './components/header/AppHeader.vue'
+import AppFooter from './components/footer/AppFooter.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/history">History</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div
+    class="min-h-screen flex flex-col bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-100"
+  >
+    <AppHeader />
+    <main class="flex-1 px-4 sm:px-6 lg:px-8 py-8">
+      <RouterView />
+    </main>
+    <AppFooter />
+  </div>
 </template>
