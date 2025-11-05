6ba9ff6f9e32824974df14cfa85fc184d939dc1c
Reese <reese.westerhoff@gmail.com>
2025-11-05 15:24:04 +0100

    use useTheme in main.ts

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -5,6 +5,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { initTheme } from './composables/useTheme'
+
+// Initialize theme before mounting
+// This ensures the dark class is applied immediately and prevents flash
+initTheme()
 
 const app = createApp(App)
 
