40b2f64f3218f74eb1472b8c52c06eb1282cb62a
Reese <reese.westerhoff@gmail.com>
2025-11-05 15:24:49 +0100

    create theme toggle interface

---
+++ front-end/src/components/theme-toggle/ThemeToggle.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useTheme } from '../../composables/useTheme'
+
+const { isDark, toggle } = useTheme()
+</script>
+
+<template>
+  <button
+    @click="toggle"
+    type="button"
+    :aria-label="isDark ? 'Switch to light mode' : 'Switch to dark mode'"
+  >
+    <span v-if="isDark" class="text-lg">☀️</span>
+    <span v-else class="text-lg">🌙</span>
+  </button>
+</template>
