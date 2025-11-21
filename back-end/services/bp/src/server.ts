e12126bb6d71180484de575e3fffbc804232d57a
Reese <reese.westerhoff@gmail.com>
2025-11-21 10:46:39 +0100

    correct front end url default

--- back-end/services/bp/src/server.ts
+++ back-end/services/bp/src/server.ts
@@ -43,7 +43,7 @@ serverApp.use(sanitizeApiInput);
 // CORS middleware
 serverApp.use(
   corsMiddleware({
-    origin: process.env.FRONTEND_URL || "http://localhost:8080",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
