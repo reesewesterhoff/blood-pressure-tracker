2af32dafb1945cc5ae45d67d9d5edbc8bad268c2
Reese <reese.westerhoff@gmail.com>
2026-01-07 14:31:34 +0100

    create loading spinner

---
+++ front-end/src/components/spinner/LoadingSpinner.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+  size?: 'sm' | 'md' | 'lg'
+}
+
+withDefaults(defineProps<Props>(), {
+  size: 'md',
+})
+
+const sizeClasses = {
+  sm: 'w-4 h-4 border-2',
+  md: 'w-8 h-8 border-2',
+  lg: 'w-12 h-12 border-4',
+}
+</script>
+
+<template>
+  <div class="flex justify-center items-center">
+    <div
+      class="border-neutral-300 dark:border-neutral-600 border-t-neutral-900 dark:border-t-neutral-100 rounded-full animate-spin"
+      :class="sizeClasses[size]"
+    />
+  </div>
+</template>
