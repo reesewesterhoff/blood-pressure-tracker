84fd7c57aaa4b958c0048fc08d708065869f7b98
Reese <reese.westerhoff@gmail.com>
2026-02-03 11:15:40 +0100

    break out into app/marketing layouts

---
+++ frontend/src/layouts/MarketingLayout.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import AppHeader from '@/components/header/AppHeader.vue'
+import AppFooter from '@/components/footer/AppFooter.vue'
+</script>
+
+<template>
+  <div
+    class="min-h-screen flex flex-col bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-100"
+  >
+    <AppHeader />
+    <main class="flex-1 px-4 sm:px-6 lg:px-8 py-8 flex flex-col">
+      <RouterView class="flex flex-col items-center w-full" />
+    </main>
+    <AppFooter />
+  </div>
+</template>
